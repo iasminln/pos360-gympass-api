c577187b73742d36dfe37ec5a42c435ed2f8f014
Iasmin <iasln@outlook.com>
2025-05-13 19:20:37 -0300

    added new repositories, created checkin test

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ npx prisma generate => gerar tipagens do typescript pro prisma
 
 # docker run --name api-solid-pg -e POSTGRESQL_USERNAME=docker -e POSTGRESQL_PASSWORD=docker -e POSTGREE_DATABASE=apisolid -p 5432:5432 bitnami/postgresql
 - Adicionado no arquivo docker-compose.yml
+
+# TDD -> Test-driven development -> desenvolvimento dirigido a testes -> TDD diz para vc que se vc desenvolve o teste para alguma regra de negócio ou alguma funcionalidade, antes da implementação, o teste te ajuda a validar se o a sua implementação está ok. é uma metodologia que facilita vc entender e caminhar pelas regras de negócio da funcionalidade durante o desenvolvimento e é muito util em casos de features mais complexas
+- red: fazer dar erro
+- green: fazer funcionar com o mínimo de código possível
+- refactor: refatorar
---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,88 @@
+import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+import { CheckInUseCase } from "./check-in";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { Decimal } from "@prisma/client/runtime/library";
+
+// testes unitários não devem acessar banco de dados e nem nada externo
+
+let checkInsRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
+let sut: CheckInUseCase;
+
+describe("Check-in Use Case", () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository();
+    gymsRepository = new InMemoryGymsRepository();
+    // sut = system under test
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository);
+
+    gymsRepository.items.push({
+      id: "gym-01",
+      title: "Teste Gym",
+      description: "",
+      phone: "",
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    });
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("should be able to check in", async () => {
+    const { checkIn } = await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it("should be able to check in twice in the same day", async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    await expect(() =>
+      sut.execute({
+        gymId: "gym-01",
+        userId: "user-01",
+        userLatitude: 0,
+        userLongitude: 0,
+      }),
+    ).rejects.toBeInstanceOf(Error);
+  });
+
+  it("should be able to check in twice but in different days", async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0));
+
+    const { checkIn } = await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+});
